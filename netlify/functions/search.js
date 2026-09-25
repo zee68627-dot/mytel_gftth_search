749bf0eea110b7e06e2ee271cd8f87b4af91f0a8
mytel-gopn <zee68627@gmail.com>
2026-09-25 14:04:27 +0630

    Update search.js

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -24,7 +24,7 @@ function convertToCSV(items) {
     "account", "subscriber_name", "custoemr_phone_number", "station_code", 
     "VMY_Code", "branch", "partner_name", "device_code", "port_on_card", 
     "port_splitter", "subscriber_node", "cable_length", "ont_serial", 
-    "technical_name", "technical_phone_number", "department", "address", "lat_long"
+    "technical_name", "technical_phone_number", "department", "address"
   ];
 
   const csvRows = [];
